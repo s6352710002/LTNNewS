25781be093ba1ad73adc888e4d2f13d69db53c7e
nichakorn <s6352710002@sau.ac.th>
2023-10-24 16:54:50 +0700

    Code V.0.2 : add and fix detail (BacktoTop)

--- js/main.js
+++ js/main.js
@@ -30,18 +30,44 @@
     
     
     // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 100) {
+    //         $('.back-to-top').fadeIn('slow');
+    //     } else {
+    //         $('.back-to-top').fadeOut('slow');
+    //     }
+    // });
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
     
+    document.addEventListener('DOMContentLoaded', function () {
+        const backToTopLink = document.getElementById('back-to-top');
+
+        // Show the link when the user scrolls down 100px from the top
+        window.addEventListener('scroll', function () {
+            if (window.pageYOffset > 100) {
+                backToTopLink.style.display = 'block';
+            } else {
+                backToTopLink.style.display = 'none';
+            }
+        });
+
+        // Smooth scroll to the top when the link is clicked
+        backToTopLink.addEventListener('click', function (e) {
+            e.preventDefault(); // หยุดการนำทางไปยัง "#" (แท็กลิงก์ที่ถูกคลิก)
+
+            const smoothScrollToTop = () => {
+                if (window.scrollY > 0) {
+                    window.scrollTo(0, window.scrollY - 15); // ปรับความเร็วที่คุณต้องการให้มันเลื่อนขึ้น
+                    requestAnimationFrame(smoothScrollToTop);
+                }
+            };
+
+            smoothScrollToTop();
+        });
+    });
     
     // Category News Slider
     $('.cn-slider').slick({
